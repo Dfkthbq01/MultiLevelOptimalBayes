7ca0b979e10fadffb51a1be868b31ac3e3997d36
Dfkthbq01 <73604346+Dfkthbq01@users.noreply.github.com>
2025-05-02 19:11:35 +0200

    Corrected note in the output

--- R/mlob.R
+++ R/mlob.R
@@ -770,21 +770,34 @@ summary.mlob_result <- function(object, ...) {
   cat("\nSignif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1\n")
   
   
-  # Compute how many % st. err. of regularized Bayesian is smaller than sr. err. of ML
-  pct_smaller <- object$Standard_Error_ML[1] / object$Standard_Error[1] * 100
+  # Compute how many times st. err. of regularized Bayesian is smaller than sr. err. of ML
+  se_ratio <- object$Standard_Error_ML[1] / object$Standard_Error[1] 
   
   # Display the note about trustworthiness of regularized Bayesian vs. ML
-  cat(
-    "\nNote:\n",
-    sprintf("  Optimized Std. Err. is %.1f%% smaller compared to the unoptimized ML Std. Err.\n",
-            pct_smaller),
-    "  The unoptimized ML estimator may differ significantly from the optimized regularized Bayesian estimator.\n",
-    "  Therefore, we strongly recommend against reporting or interpreting ML estimation results.\n",
-    "  Always rely on optimized estimates, as they provide more accurate and reliable insights.\n",
-    "  For further methodological details, refer to Dashuk et al. (2025).\n",
-    sep = ""
-  )
+  cat("\nNote:\n")
 
+  # Display the appropriate note based on standard error comparison
+  # If the ratio between standard errors is <= times 1.1, treat them as similarly accurate
+  
+  if (se_ratio <= 1.1) {
+    cat("  The standard error from unoptimized ML estimation is approximately the same \n",
+        "  as the standard error obtained through our optimization procedure,\n",
+        "  meaning that both approaches yield similarly accurate estimates.\n", sep = "")
+    
+  } else {
+    cat(
+      sprintf("    The standard error from unoptimized ML estimation is about %.1f times  larger \n", 
+              se_ratio),
+      "  than the standard error obtained through our optimization procedure, \n",
+      "  meaning that the optimized estimates are more accurate. \n", 
+      "    Concerning the estimates themselves, the unoptimized ML estimates may differ \n", 
+      "  greatly from the optimized estimates and should not be reported. \n",
+      "    As the optimized estimates are always at least as accurate as \n", 
+      "  the unoptimized ML estimates, please use them and their corresponding \n", 
+      "  standard errors (first table of output) for interpretation and reporting. \n",
+      "    For more information, see Dashuk et al. (2025). \n",
+      sep = "")
+  }
 }
 
 
